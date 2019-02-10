508e145759ad6994b37e6ec81826d8eeab9881ef
gness1804 <graham.nessler@gmail.com>
2019-02-10 16:09:32 -0600

    WIP: got market names coming in

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Error = () => <div>Error</div>;
+const Error = ({ errors }) => {
+  const errorsElem = errors.map(({ message }, i) => (
+    <div key={i}>{message}</div>
+  ));
+  return <pre className="error">{errorsElem}</pre>;
+};
 
 export default Error;
--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -1,8 +1,40 @@
 import React from 'react';
-// import { Loading, Card, Icon, Tag } from "element-react";
+import { Connect } from 'aws-amplify-react';
+import { graphqlOperation } from 'aws-amplify';
+// prettier-ignore
+import {
+  Loading,
+  Card,
+  Icon,
+  Tag,
+} from 'element-react';
+import { listMarkets } from '../graphql/queries';
+import Error from './Error';
 
 const MarketList = () => {
-  return <div>MarketList</div>;
+  return (
+    <Connect query={graphqlOperation(listMarkets)}>
+      {({ data, loading, errors }) => {
+        // check if there are any errors or in loading state or markets do not exist in data
+        if (errors.length > 0) {
+          return <Error errors={errors} />;
+        }
+        if (loading || !data || !data.listMarkets) {
+          return <Loading fullscreen />;
+        }
+
+        const { items: markets } = data.listMarkets;
+        return (
+          <>
+            {markets.map(market => (
+              // TODO: replace with actual markup
+              <div>{market.name}</div>
+            ))}
+          </>
+        );
+      }}
+    </Connect>
+  );
 };
 
 export default MarketList;
