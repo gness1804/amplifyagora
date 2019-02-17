43f550eb7bc9e9231ba1d9138d26fab23f012429
gness1804 <graham.nessler@gmail.com>
2019-02-16 18:22:05 -0600

    Got market rendering on page

--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,8 +1,53 @@
 import React from 'react';
-// import { Loading, Tabs, Icon } from "element-react";
+import { API, graphqlOperation } from 'aws-amplify';
+// prettier-ignore
+import {
+  Loading,
+  Tabs,
+  Icon,
+  Notification,
+} from 'element-react';
+import { getMarket } from '../graphql/queries';
 
 class MarketPage extends React.Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      market: null,
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.handleGetMarket();
+  }
+
+  handleGetMarket = async () => {
+    const { marketId: id } = this.props;
+    try {
+      const input = {
+        id,
+      };
+
+      const result = await API.graphql(graphqlOperation(getMarket, input));
+      this.setState({
+        market: result.data.getMarket,
+        isLoading: false,
+      });
+    } catch (err) {
+      /* eslint-disable no-console */
+      console.error(
+        `Error fetching market with id ${id}: ${err.message ||
+          JSON.stringify(err)}`,
+      );
+      Notification.error({
+        title: 'Error',
+        message: `Error adding market: ${err.message || JSON.stringify(err)}`,
+      });
+      /* eslint-enable no-console */
+    }
+  };
 
   render() {
     return <div>Market Page with id: {this.props.marketId}</div>;
